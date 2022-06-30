5d6d62fe0514478cf9d4b79ba8c812a4a4c61856
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-30 19:23:38 +0600

    putting task done

--- index.js
+++ index.js
@@ -11,13 +11,35 @@ app.use(cors());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.us6b7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+const tasksCollection = client.db("tasks-db").collection("tasks-collection");
+const run = async () => {
+    try {
+        await client.connect();
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+        app.get('/alltasks', async (req, res) => {
+            const tasks = await tasksCollection.find({}).toArray();
+            res.send(tasks)
+        })
+
+        app.put('/addoredit', async (req, res) => {
+            const id = ObjectId(req.headers.id);
+            const task = req.body;
+            const doc = {
+                $set: task
+            }
+            const result = await tasksCollection.updateOne({ _id: id }, doc, { upsert: true });
+            console.log(id, task, result)
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.log(e.message))
 
 
 app.get('/', (req, res) => {
